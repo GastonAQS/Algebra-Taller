82b096192e4ef28efc0521139efd9ac9a6260e9e
GastonAQS <gastonq98@gmail.com>
2020-09-24 20:33:05 -0300

    nuevas funciones

--- clase04.hs
+++ clase04.hs
@@ -2,7 +2,17 @@ sumatoria1 :: Int -> Int
 sumatoria1 0 = 1
 sumatoria1 n = 2^n + sumatoria1 (n-1)
 
-sumatoria2 :: (Fractional a, Eq a, Integral b, Eq b) => a -> b -> a
-sumatoria2 1 _ = -1
-sumatoria2 q 1 = (1-q^1)/(1-q)
-sumatoria2 q n = (1-q^n)/(1-q) + sumatoria2 q (n-1)
+sumatoria2 :: Float -> Int -> Float
+sumatoria2 q 0 = 0
+sumatoria2 q n = q^n + sumatoria2 q (n-1)
+
+sumatoria3' :: Float -> Int -> Float
+sumatoria3' q n = sumatoria2 q (2*n)
+
+sumatoria3 :: Float -> Int -> Float
+sumatoria3 q 0 = 0
+sumatoria3 q n = sumatoria3 q (n-1) + q^(2*n-1) + q^(2*n)
+
+sumatoria4 :: Float -> Int -> Float
+sumatoria4 q 0 = 1
+sumatoria4 q n = q^(2*n-1) + q^(2*n) - q^(n-1) + sumatoria4 q (n-1)
