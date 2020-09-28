9b1517d5241b843d929a27948d0f81fc506657d6
GastonAQS <gastonq98@gmail.com>
2020-09-28 02:42:47 -0300

    nuevos ejercicios clase 4

--- clase04.hs
+++ clase04.hs
@@ -1,3 +1,7 @@
+sumatoria :: Int -> Int
+sumatoria 0 = 0
+sumatoria n = n + sumatoria (n-1)
+
 sumatoria1 :: Int -> Int
 sumatoria1 0 = 1
 sumatoria1 n = 2^n + sumatoria1 (n-1)
@@ -16,3 +20,30 @@ sumatoria3 q n = sumatoria3 q (n-1) + q^(2*n-1) + q^(2*n)
 sumatoria4 :: Float -> Int -> Float
 sumatoria4 q 0 = 1
 sumatoria4 q n = q^(2*n-1) + q^(2*n) - q^(n-1) + sumatoria4 q (n-1)
+
+factorial :: Int -> Int
+factorial 1 = 1
+factorial n = n*(factorial (n-1))
+
+eAprox :: Int -> Float
+eAprox 0 = 1
+eAprox n = (1/(fromIntegral (factorial n))) + eAprox (n-1)
+
+e :: Float
+e = eAprox 10
+
+f :: Int -> Int -> Int
+f n 0 = 0
+f n m = (f (n-1) m) + round (sumatoria2 (fromIntegral n) m)
+
+sumaPotencias :: Float -> Int -> Int -> Float
+sumaPotencias q n 0 = 0
+sumaPotencias q n m = (q^m*(sumatoria2 q n)) + sumaPotencias q n (m-1)
+
+sumaRacionales :: Int -> Int -> Float
+sumaRacionales n 0 = 0
+sumaRacionales n m = (fromIntegral (sumatoria n))/(fromIntegral m) + sumaRacionales n (m-1)
+
+tarea4 :: Int -> Int -> Int
+tarea4 i n | i==n = 0
+tarea4 i n = i^i + tarea4 i (n-1)
